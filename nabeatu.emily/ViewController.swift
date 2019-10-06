f03401ef1995f80d9e0336d04a49c13611e82ccf
appleEmily <emiri3128@gmail.com>
2019-10-07 01:58:48 +0900

    ナベアツ的な

--- nabeatu.emily/ViewController.swift
+++ nabeatu.emily/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number:Int = 0
+    
+    @IBOutlet weak var picture: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func plus(_ sender: Any) {
+        number = number + 1
+        label.text = String(number)
+        if number / 10 % 3 == 1 {
+            picture.image = UIImage(named: "NYC.jpg")
+        } else if number % 3 == 1 {
+            picture.image = UIImage(named: "pari.jpg")
+        } else {
+            picture.image = UIImage(named: "pari2.jpg")
+        }
+    }
+    @IBAction func clear(_ sender: Any) {
+        number = 0
+        label.text = String(number)
+        picture.image = UIImage(named:"pari.jpg")
+    }
+    
 
 }
 
